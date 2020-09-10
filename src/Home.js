9e3b3169a6bf40af9520166fef15a1fe283bf213
Duan Chandrababu <Duan.Chandrababu@cammsgroup.com>
2020-09-10 12:34:09 +0530

    NavBar

---
+++ src/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Home.css";
+
+const Home = () => {
+  return (
+    <div className="home__container">
+      <img
+        src="https://m.media-amazon.com/images/G/01/digital/video/sonata/Superhero_UK_Acquisition_FT_Apr_20/f4b165f5-678a-48bb-a049-11e7ddc2168d._UR3000,600_SX1500_FMjpg_.jpg"
+        alt=""
+      />
+    </div>
+  );
+};
+
+export default Home;
---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyCgYv_xMmBsKO-j07HzHlg-NsVoVy3ymts",
+  authDomain: "clone-3c06f.firebaseapp.com",
+  databaseURL: "https://clone-3c06f.firebaseio.com",
+  projectId: "clone-3c06f",
+  storageBucket: "clone-3c06f.appspot.com",
+  messagingSenderId: "395240364268",
+  appId: "1:395240364268:web:bb1571f7cfa07d8dd1adc9",
+  measurementId: "G-X3KHMY8RMG",
+};

e600b9e473facd05e0064062c84e8803ebc08a2b
Duan Chandrababu <Duan.Chandrababu@cammsgroup.com>
2020-09-30 14:51:38 +0530

    checkout

--- src/Checkout.js
+++ src/Checkout.js
@@ -2,11 +2,21 @@ import React from "react";
 import "./Checkout.css";
 import SubTotal from "./SubTotal";
 import { useStateValue } from "./StateProvider";
+import CheckoutProduct from "./CheckoutProduct";
 
 const Checkout = () => {
   const [{ basket }, dispatch] = useStateValue();
-  var checkoutList = basket.map((item,index)=>{
-    return <label key={index}>{item.title}</label>
+  var checkoutList = basket.map((item, index) => {
+    return (
+      <CheckoutProduct
+        id={item.id}
+        title={item.title}
+        price={item.price}
+        image={item.image}
+        rating={item.rating}
+        key={index}
+      />
+    );
   });
   return (
     <div className="checkout">
